8ab42f63c53eb588a44d8764172b430b0c5a3da9
guibenat <guibenattedev@gmail.com>
2026-09-09 00:20:42 -0300

    feat: Adding toFixed to the results

--- js/calculator.js
+++ js/calculator.js
@@ -8,7 +8,7 @@ function simular() {
     let custoHoraParada = Number(ipt_custoHoraParada.value)
 
     // Calculos
-    
+
     let massaPerdida = insumo * (perdaTransbordo / 100); 
 
     let produtoNaoGerado = massaPerdida * (rendimento / 100);
@@ -31,15 +31,15 @@ function simular() {
     // Mostrar resultado
 
     if (insumo > 0 && rendimento > 0 && perdaTransbordo > 0 && valorProd > 0 && horasParadas > 0 && custoHoraParada > 0) {
-        h2_prejuizo_anual.innerHTML = `${prejuizoAnual}`
-        p_massaPerdida.innerHTML = `${massaPerdida}`
-        p_produtoNaoGerado.innerHTML = `${produtoNaoGerado}`
-        p_prejuizoPorTransbordo.innerHTML = `${prejuizoTransbordo}`
-        p_prejuizoPorParada.innerHTML = `${prejuizoDowntime}`
-        p_ganhoDireto.innerHTML = `${ganhoDireto}`
-        h1_recuperadoPorAno.innerHTML = `${ganhoDireto}`
-        p_economiaUmAno.innerHTML = `${ano1}`
-        p_economiaTresAnos.innerHTML = `${ano3}`
+        h2_prejuizo_anual.innerHTML = prejuizoAnual.toFixed(2);
+        p_massaPerdida.innerHTML = massaPerdida.toFixed(2);
+        p_produtoNaoGerado.innerHTML = produtoNaoGerado.toFixed(2);
+        p_prejuizoPorTransbordo.innerHTML = prejuizoTransbordo.toFixed(2);
+        p_prejuizoPorParada.innerHTML = prejuizoDowntime.toFixed(2);
+        p_ganhoDireto.innerHTML = ganhoDireto.toFixed(2);
+        h1_recuperadoPorAno.innerHTML = ganhoDireto.toFixed(2);
+        p_economiaUmAno.innerHTML = ano1.toFixed(2);
+        p_economiaTresAnos.innerHTML = ano3.toFixed(2);
     } else {
         h2_prejuizo_anual.innerHTML = `Insira os valores corretamente`
     }
